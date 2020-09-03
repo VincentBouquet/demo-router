82ea776089da7bc211ab22079fa66059c3d5fcfb
fcplus.r72 <bouquet.vincent704@orange.fr>
2020-09-03 11:18:10 +0200

    1rst

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import HomePage from "./pages/HomePage"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+          <Link to="/bonjour/1">Lien 1</Link>
+          <Link to="/bonjour/2">Lien 2</Link>
+          <Link to="/bonjour/3">Lien 3</Link>
+        <Switch>
+            <Route exact path="/">
+                <HomePage/> {/*<h1>HomePage</h1>*/}
+            </Route>
+          <Route path="/bonjour/1">
+            <h1>Salut</h1>
+          </Route>
+            <Route path="/bonjour/2">
+                <h1>à</h1>
+            </Route>
+            <Route path="/bonjour/3">
+                <h1>tous</h1>
+            </Route>
+            <Route>
+                <h1>404</h1>
+            </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
